ccb7959114fc8e78815b1b6d525c091b14463b8f
sanjildev <shakyasanjil00@gmail.com>
2025-05-06 07:44:56 +0545

    navbar

--- src/pages/AddBlog.jsx
+++ src/pages/AddBlog.jsx
@@ -37,33 +37,33 @@ setData({...data,[name]:name==="image" ? e.target.files[0] :value})
   return (
   <>
   <Navbar/>
-         <form class="max-w-md  mx-auto mt-20 p-6 bg-white border rounded-lg shadow-lg" onSubmit={createBlog}>
-    <h2 class="text-2xl text-center font-bold mb-6">Add Blog</h2>
-    <div class="mb-4">
-        <label class="block text-gray-700 font-bold mb-2" for="title">
+         <form className="max-w-md  mx-auto mt-20 p-6 bg-white border rounded-lg shadow-lg" onSubmit={createBlog}>
+    <h2 className="text-2xl text-center font-bold mb-6">Add Blog</h2>
+    <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" for="title">
       Title:
     </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" type="text" placeholder="Enter your title" name="title" onChange={handleChange}/>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" type="text" placeholder="Enter your title" name="title" onChange={handleChange}/>
     </div>
-    <div class="mb-4">
-        <label class="block text-gray-700 font-bold mb-2" for="subtitle">
+    <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" for="subtitle">
       Subtitle:
     </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="subtitle" type="text" placeholder="Enter your subtitle" name="subtitle"  onChange={handleChange}/>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="subtitle" type="text" placeholder="Enter your subtitle" name="subtitle"  onChange={handleChange}/>
     </div>
-    <div class="mb-4">
-        <label class="block text-gray-700 font-bold mb-2" for="description">
+    <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" for="description">
      Description:
     </label>
-        <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" rows="5" placeholder="Enter your description" name="description" onChange={handleChange}></textarea>
+        <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" rows="5" placeholder="Enter your description" name="description" onChange={handleChange}></textarea>
     </div>
-    <div class="mb-4">
-        <label class="block text-gray-700 font-bold mb-2" for="image">
+    <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" for="image">
      Image
     </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="image" type="file" name="image" placeholder="Enter your image" onChange={handleChange}/>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="image" type="file" name="image" placeholder="Enter your image" onChange={handleChange}/>
     </div>
-    <button class="bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer" type="submit">
+    <button className="bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer" type="submit">
     Add
   </button>
 </form>
